5d1eab6cdaf4edf6aa2b110ea02efe6b636f509c
unknown <rakibulanas033@gmail.com>
2023-05-25 01:34:50 +0600

    add

--- src/component/Testimonial.js
+++ src/component/Testimonial.js
@@ -47,79 +47,76 @@ const Testimonial = () => {
 
 				<div className=" relative z-10">
 					<Slider {...settings}>
-						<div className="p-4">
-							<div className="h-72 flex flex-col justify-between testimonial p-8 rounded">
-								<div>
-									<img
-										src="/assets/fontisto_quote-a-right.png"
-										className="mb-2"
-										alt=""
-									/>
-									<p className=" mb-8 text-xs">
-										"RemoteHireNow has saved us thousands of dollars on hiring
-										and growing my team quickly. I'd recommend them to any
-										business looking to outsource."
-									</p>
-								</div>
-								<div className="flex justify-between items-center">
-									<a className="inline-flex items-center">
-										<img
-											alt="testimonial"
-											src="/assets/Ellipse 5.png"
-											className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-										/>
-										<span className=" pl-4">
-											<span className="title-font font-medium text-white">
-												Torch Labs Software LLC
-											</span>
-										</span>
-									</a>
-									<div className="flex justify-between items-center">
-										<AiFillStar className="text-white" />
-										<AiFillStar className="text-white" />
-										<AiFillStar className="text-white" />
-										<AiFillStar className="text-white" />
-										<AiFillStar className="text-white" />
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="p-4">
-							<div className="h-72 testimonial p-8 rounded">
+						<div className="h-72 flex flex-col justify-between testimonial p-8 rounded">
+							<div>
 								<img
 									src="/assets/fontisto_quote-a-right.png"
 									className="mb-2"
 									alt=""
 								/>
 								<p className=" mb-8 text-xs">
-									“RemoteHireNow surpassed my expectations with their
-									personalized attention and commitment to understanding our
-									company's unique needs. Their professionalism throughout the
-									hiring process was exceptional, and they provided a remarkable
-									pool of qualified candidates. Thanks to their efforts, we
-									successfully filled key positions and their ongoing support
-									has been invaluable to our team's success.”
-								</p>{" "}
-								<div className="flex justify-between items-center">
-									<a className="inline-flex items-center">
-										<img
-											alt="testimonial"
-											src="/assets/Ellipse 5.png"
-											className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-										/>
-										<span className=" pl-4">
-											<span className="title-font font-medium text-white">
-												Torch Labs Software LLC
-											</span>
+									"RemoteHireNow has saved us thousands of dollars on hiring and
+									growing my team quickly. I'd recommend them to any business
+									looking to outsource."
+								</p>
+							</div>
+							<div className="flex justify-between items-center">
+								<a className="inline-flex items-center">
+									<img
+										alt="testimonial"
+										src="/assets/Ellipse 5.png"
+										className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
+									/>
+									<span className=" pl-4">
+										<span className="title-font font-medium text-white">
+											Torch Labs Software LLC
 										</span>
-									</a>
-									<div className="flex justify-between items-center">
-										<AiFillStar className="text-white" />
-										<AiFillStar className="text-white" />
-										<AiFillStar className="text-white" />
-										<AiFillStar className="text-white" />
-										<AiFillStar className="text-white" />
-									</div>
+									</span>
+								</a>
+								<div className="flex justify-between items-center">
+									<AiFillStar className="text-white" />
+									<AiFillStar className="text-white" />
+									<AiFillStar className="text-white" />
+									<AiFillStar className="text-white" />
+									<AiFillStar className="text-white" />
+								</div>
+							</div>
+						</div>
+
+						<div className="h-72 testimonial p-8 rounded">
+							<img
+								src="/assets/fontisto_quote-a-right.png"
+								className="mb-2"
+								alt=""
+							/>
+							<p className=" mb-8 text-xs">
+								“RemoteHireNow surpassed my expectations with their personalized
+								attention and commitment to understanding our company's unique
+								needs. Their professionalism throughout the hiring process was
+								exceptional, and they provided a remarkable pool of qualified
+								candidates. Thanks to their efforts, we successfully filled key
+								positions and their ongoing support has been invaluable to our
+								team's success.”
+							</p>{" "}
+							<div className="flex justify-between items-center">
+								<a className="inline-flex items-center">
+									<img
+										alt="testimonial"
+										src="/assets/Ellipse 5.png"
+										className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
+									/>
+									<span className=" pl-4">
+										<span className="title-font font-medium text-white">
+											Torch Labs Software LLC
+										</span>
+									</span>
+								</a>
+								<div className="flex justify-between items-center">
+									<AiFillStar className="text-white" />
+									<AiFillStar className="text-white" />
+									<AiFillStar className="text-white" />
+									<AiFillStar className="text-white" />
+									<AiFillStar className="text-white" />
 								</div>
 							</div>
 						</div>
